08deac9804fdb06b940ab5465caf2bbd3c06cfcd
dobriytank <38687452+dobriytank@users.noreply.github.com>
2018-07-22 08:58:33 +0300

    jjjj

--- ASA_config.py
+++ ASA_config.py
@@ -4,21 +4,10 @@ import sys
 
 file = open('IP', 'r')
 
+
 USER = input('Username: ')
 PASSWORD = getpass.getpass()
 ENABLE_PASS = getpass.getpass(prompt='Enter enable password: ')
-DEVICES_IP = ['10.78.16.4','10.78.16.4']
+DEVICES_IP = file.read().split('\n')
+print(DEVICES_IP)
 
-for IP in DEVICES_IP:
-    print('Connection to device {}'.format(IP))
-    DEVICE_PARAMS = {'device_type': 'cisco_asa',
-                     'ip': IP,
-                     'username': USER,
-                     'password': PASSWORD,
-                     'secret': ENABLE_PASS}
-
-    with ConnectHandler(**DEVICE_PARAMS) as ssh:
-        ssh.enable()
-
-        #result = ssh.send_command(COMMAND)
-        #print(result)
