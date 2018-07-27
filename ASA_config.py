7882543d30aefceee24f11e05682cd6af7bdafcf
dobriytank <38687452+dobriytank@users.noreply.github.com>
2018-07-27 23:58:17 +0300

    111

--- ASA_config.py
+++ ASA_config.py
@@ -8,6 +8,7 @@ PASSWORD = getpass.getpass()
 ENABLE_PASS = getpass.getpass(prompt='Enter enable password: ')
 DEVICES_IP = file.read().split('\n')
 print(DEVICES_IP)
+file.close()
 
 for line in DEVICES_IP:
     ip_address = line.strip()
@@ -21,7 +22,28 @@ for line in DEVICES_IP:
         'port': 22,
     }
     net_connect = ConnectHandler(**device)
-    output = net_connect.send_command('show access-list in_Factory | i deny ip any any')
-    hostname = net_connect.send_command('show version')
-    print(hostname, output)
+    net_connect.config_mode()
+    output = net_connect.send_command('show run | i access-group in_ Office')
+    hostname = net_connect.send_command('show hostname')
+    if output!="":
+        print(hostname, output)
+    else:
+        print(hostname, 'No such string')
     net_connect.disconnect()
+
+
+#def write_to_file(multiple, output_file, input_parse, input_names, input_objects, input_object_groups,
+ #                     input_access_lists, input_object_nat, input_static_nat, ip_address):
+#
+ #       if multiple:
+  #          filename = ip_address.split('.')[0] + "-" + ip_address.split('.')[1] + "-" + ip_address.split('.')[
+   #             2] + "-" + ip_address.split('.')[3] + ".txt"
+    #        f = open(filename, 'w')
+     #   else:
+      #      if output_file:
+       #         f = open(output_file, 'w')
+        #    else:
+         #       f = open("output.txt", 'w')
+    #    f.write("Output returned for asa_ip.py script\n")
+     #   f.write("IP Address Provided:  " + ip_address + "\n")
+      #  f.write("Date:  " + today.ctime() + "\n\n\n")
