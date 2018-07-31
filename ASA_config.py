a8f604bb903ce124c6362ec2ed4aeef0f87813fa
dobriytank <38687452+dobriytank@users.noreply.github.com>
2018-07-31 08:20:52 +0300

    1111

--- ASA_config.py
+++ ASA_config.py
@@ -2,31 +2,32 @@ from netmiko import ConnectHandler
 import getpass
 import time
 import sys
-file = open('IP', 'r')
 
+file = open('IP', 'r')
 USER = input('Username: ')
 PASSWORD = getpass.getpass()
 ENABLE_PASS = getpass.getpass(prompt='Enter enable password: ')
-DEVICES_IP = file.read().split('\n')
+DEVICES_IP = file.read().splitlines()
 print(DEVICES_IP)
 file.close()
 result = open('result', 'w')
 
+
 for line in DEVICES_IP:
+ #   if line[0] == "#": continue
     ip_address = line.strip()
- #   print(ip_address)
     device = {
-        'device_type': 'cisco_asa',
-        'ip': ip_address,
-        'username': USER,
-        'password': PASSWORD,
-        'secret': ENABLE_PASS,
-        'port': 22,
+    'device_type': 'cisco_asa',
+    'ip': ip_address,
+    'username': USER,
+    'password': PASSWORD,
+    'secret': ENABLE_PASS,
+    'port': 22,
     }
-    net_connect = ConnectHandler(**device)
-#    time.sleep(3)
+    net_connect = ConnectHandler(**device, global_delay_factor=2)
 #    net_connect.config_mode()
-    output = net_connect.send_command('show run | i access-group in_Office')
+    output = net_connect.send_command('show running-config | include access-group in_Office')
+    time.sleep(2)
     hostname = net_connect.send_command('show hostname')
 
     if output!="":
@@ -34,22 +35,8 @@ for line in DEVICES_IP:
         result.write(hostname + ip_address+'\n' + output+'\n')
     else:
         print(hostname, 'No such string')
-        result.write(hostname + 'No such string'+'\n')
+        result.write(hostname + ip_address+'\n' + 'No such string'+'\n')
     net_connect.disconnect()
+    time.sleep(2)
 
 result.close()
-#def write_to_file(multiple, output_file, input_parse, input_names, input_objects, input_object_groups,
- #                     input_access_lists, input_object_nat, input_static_nat, ip_address):
-#
- #       if multiple:
-  #          filename = ip_address.split('.')[0] + "-" + ip_address.split('.')[1] + "-" + ip_address.split('.')[
-   #             2] + "-" + ip_address.split('.')[3] + ".txt"
-    #        f = open(filename, 'w')
-     #   else:
-      #      if output_file:
-       #         f = open(output_file, 'w')
-        #    else:
-         #       f = open("output.txt", 'w')
-    #    f.write("Output returned for asa_ip.py script\n")
-     #   f.write("IP Address Provided:  " + ip_address + "\n")
-      #  f.write("Date:  " + today.ctime() + "\n\n\n")
--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-device = {
-    'device_type': 'cisco_asa',
-    'ip': ip_address,
-    'username': USER,
-    'password': PASSWORD,
-    'port': 22,
-}
-
-net_connect = ConnectHandler(**device)
-output = net_connect.send_command_expect('show version')
-print output
+DEVICES_IP = file.read().split('\n')
